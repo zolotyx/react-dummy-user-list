6f154f98821405b1ec3068e20be15c791307a0c8
Zolotyx <zolotyx@gmail.com>
2017-12-07 00:33:07 +0700

    Unused css file removed

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import * as ReactDOM from 'react-dom';
 import { App } from './app/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
 
 ReactDOM.render(
   <App/>,
